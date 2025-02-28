b3c338ad20c9be49c6e26cacea61c7e5d294ae71
Joel Burlingame <jtburlingame777@gmail.com>
2025-02-28 07:04:53 -0800

    merged

--- prog.cpp
+++ prog.cpp
@@ -1,50 +1,36 @@
-
-/**
- * Assignment 2: Simple UNIX Shell
- * @file pcbtable.h
- * @author Joel Burlingame (TODO: your name)
- * @brief This is the main function of a simple UNIX Shell. You may add additional functions in this file for your implementation
- * @version 0.1
- */
-// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
-
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
 #include <fcntl.h>
 #include <cstring>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <cstdlib>
 
 using namespace std;
 
-#define MAX_LINE 80 // The maximum length command
+#define MAX_LINE 80  // The maximum length command
 #define MAX_HISTORY 10
 
 char *history[MAX_HISTORY];
 int history_count = 0;
 
 /**
- * @brief parse out the command and arguments from the input command separated by spaces
+ * @brief Parse out the command and arguments from the input command separated by spaces
  *
  * @param command
  * @param args
  * @return int
  */
-int parse_command(char command[], char *args[])
-{
+int parse_command(char command[], char *args[]) {
     int count = 0;
     char *token = strtok(command, " \n");
-    while(token != nullptr){
+    while (token != nullptr) {
         args[count++] = token;
         token = strtok(nullptr, " \n");
     }
-    args[count] = nullptr;
+    args[count] = nullptr;  // Null-terminate the array
     return count;
 }
-
-// TODO: Add additional functions if you need
 
 /**
  * @brief The main function of a simple UNIX Shell. You may add additional functions in this file for your implementation
@@ -52,62 +38,69 @@ int parse_command(char command[], char *args[])
  * @param argv The array of arguments
  * @return The exit status of the program
  */
-int main(int argc, char *argv[])
-{
-    char command[MAX_LINE];       // the command that was entered
-    char *args[MAX_LINE / 2 + 1]; // hold parsed out command line arguments
-    int should_run = 1;           /* flag to determine when to exit program */
+int main(int argc, char *argv[]) {
+    char command[MAX_LINE];       // The command that was entered
+    char *args[MAX_LINE / 2 + 1]; // Hold parsed out command line arguments
+    int should_run = 1;           // Flag to determine when to exit program
 
-    // TODO: Add additional variables for the implementation.
-
-    while (should_run)
-    {
-        printf("osh>");
+    while (should_run) {
+        printf("osh> ");
         fflush(stdout);
+
         // Read the input command
-        if(fgets(command, MAX_LINE, stdin) == nullptr){
+        if (fgets(command, MAX_LINE, stdin) == nullptr) {
             continue;
         }
+
         // Parse the input command
         int num_args = parse_command(command, args);
 
-        if(num_args == 0){
-            continue;
-        }
-        if(strcmp(args[0], "exit") == 0){
-            should_run = 0;
+        //args check
+        if (num_args == 0) {
             continue;
         }
 
-        if(strcmp(args[0], "!!\n") == 0){
-            if(history_count == 0){
+        // Handle "exit" command
+        if (strcmp(args[0], "exit") == 0) {
+            should_run = 0;
+            break; 
+        }
+        //history check
+        if (strcmp(args[0], "!!") == 0) {
+            if (history_count == 0) {
                 printf("No commands in history.\n");
                 continue;
             }
             strcpy(command, history[history_count - 1]);
-            printf("%s", command);
-            parse_command(command, args);
-        }
-        if (history_count < MAX_HISTORY) {
-            history[history_count] = strdup(command);
-            history_count++;
-        } 
-        else{
-            free(history[0]);
-            for(int i = 1; i < MAX_HISTORY; i++){
-                history[i - 1] = history[i];
+            printf("%s\n", command);
+            command[strcspn(command, "\n")] = '\0';
+            num_args = parse_command(command, args);
+
+        }else{
+                // Store command in history
+            if (history_count < MAX_HISTORY) {
+                history[history_count] = strdup(command);
+                history_count++;
             }
-             history[MAX_HISTORY - 1] = strdup(command);
+            else {
+                free(history[0]);  // Free the oldest entry
+                    for (int i = 1; i < MAX_HISTORY; i++) {
+                    free(history[i - 1]);
+                    history[i - 1] = history[i];
+                }
+                history[MAX_HISTORY - 1] = strdup(command);
+            }
         }
+
         
+
         pid_t pid = fork();
 
-        if(pid < 0){
-            perror("Fork failded");
+        if (pid < 0) {
+            perror("Fork failed");
             continue;
-        }
-        else if(pid == 0){
-
+            
+        } else if (pid == 0) {  // Child process
             int fd;
             for (int i = 0; i < num_args; i++) {
                 if (strcmp(args[i], ">") == 0) { // Output Redirection
@@ -116,42 +109,39 @@ int main(int argc, char *argv[])
                         perror("Error opening file");
                         exit(EXIT_FAILURE);
                     }
-                    dup2(fd, STDOUT_FILENO); // Redirect standard output to file
+                    dup2(fd, STDOUT_FILENO);  // Redirect standard output to file
                     close(fd);
-                    args[i] = nullptr; // Remove ">" from arguments
+                    args[i] = nullptr;  // Remove ">" from arguments
                     break;
-                }
-                else if (strcmp(args[i], "<") == 0) { // Input Redirection
+                } else if (strcmp(args[i], "<") == 0) { // Input Redirection
                     fd = open(args[i + 1], O_RDONLY);
                     if (fd == -1) {
                         perror("Error opening file");
                         exit(EXIT_FAILURE);
                     }
-                    dup2(fd, STDIN_FILENO); // Redirect standard input from file
+                    dup2(fd, STDIN_FILENO);  // Redirect standard input from file
                     close(fd);
-                    args[i] = nullptr; // Remove "<" from arguments
+                    args[i] = nullptr;  // Remove "<" from arguments
                     break;
                 }
             }
-            if(execvp(args[0], args) == -1){
+            if (execvp(args[0], args) == -1) {
                 perror("Error execution");
                 exit(EXIT_FAILURE);
             }
         }
-        
-        else{
+
+        // Parent handle background execution
+        else {
             int background = (num_args > 0 && strcmp(args[num_args - 1], "&") == 0);
-            if(background){
-                args[num_args - 1] = nullptr;
-            }
-            else{
+            if (background) {
+                args[num_args - 1] = nullptr;  // Remove "&"
+                num_args--;
+            } else {
                 wait(nullptr);
             }
         }
     }
-    for(int i = 0; i < history_count; i++){
-        free(history[i]);
-    }
-    
+    //test
     return 0;
 }
